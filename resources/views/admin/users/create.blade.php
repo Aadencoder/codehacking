61dc99455a3f27b03aa38cfc1a8f29af1b5bc1af
DESKTOP-HEA3A3E\Aaden Sah <contactaaden101@gmail.com>
2017-09-27 13:49:45 +0545

    User create completed

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -30,7 +30,7 @@
 			
 			<div class="form-group">
 				{!! Form::label('file', 'File :') !!}
-				{!! Form::file('file', null, ['class'=>'form-control']) !!}
+				{!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
 			</div>
 
 			<div class="form-group">
